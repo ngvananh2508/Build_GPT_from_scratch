c77610f3f6356c4aa84b8ef974ef6c344d30df25
Van-Anh Nguyen <ngvananh25897@gmail.com>
2025-06-18 17:30:58 +0700

    second commit

--- GPT.py
+++ GPT.py
@@ -197,5 +197,3 @@ for iter in range(max_iters):
 
 context = torch.zeros((1,1), dtype=torch.long, device=device)
 print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-
